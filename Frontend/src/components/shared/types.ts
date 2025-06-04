46b0b0d0a2ef890872b2d24bfdb225805499e0eb
Vansh1379 <vanshkalra1379@gmail.com>
2025-06-04 07:21:13 +0530

    Added changes

--- Frontend/src/components/shared/types.ts
+++ Frontend/src/components/shared/types.ts
@@ -1,10 +1,6 @@
 export type Product = {
   id: string;
   name: string;
-  description: string;
-  price: number;
   category: string;
   imageUrl: string;
-  stock: number;
-  createdAt: string;
 };
